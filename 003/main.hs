87848356505c62710ea0b2ae348a9bc4e03d5a8b
Carlos Valiente <carlos@pepelabs.net>
2013-08-01 08:51:43 +0100

    Problems 2 and 3

---
+++ 003/main.hs
@@ -0,0 +1,17 @@
+-- The prime factors of 13195 are 5, 7, 13 and 29.
+-- 
+-- What is the largest prime factor of the number 600851475143?
+
+factorBound :: Integer -> Integer
+factorBound = floor . sqrt . fromIntegral
+
+sieveFactors :: Integer -> [Integer]
+sieveFactors n = sieve0 [2 .. b] where
+  b = factorBound n
+  -- ``sieve0``: All divisors of ``n`` in the Eratosthenes sieve, 
+  -- up to ``floor(sqrt(n))``
+  sieve0 [] = []
+  sieve0 (x:xs) = x : sieve0 (filter (\i -> i `mod` x /= 0 && n `mod` i == 0 ) xs)
+  
+main = do
+  print $ sieveFactors 600851475143
